d873a953f0ec72dc91b2beac98d07ef4efc9095a
Akhil870 <72069148+Akhilesh0947@users.noreply.github.com>
2022-04-05 07:48:47 +0530

    Update PassWord_Generator.cpp

--- PassWord_Generator.cpp
+++ PassWord_Generator.cpp
@@ -1,1 +1,43 @@
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
 
+string Password_Generator(int Length){
+
+  //Creating Input Variable
+  string BigChar="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  string SmallChar="abcdefghijklmnopqrstuvwxyz";
+  string Numbers="0123456789";
+  string Symbols="!~@#$%^&*";
+
+  // Add all Variable in one string
+  string temp=BigChar+SmallChar+Numbers+Symbols;
+
+  //To store Random PassWord
+  string PassWord="";
+
+  for (int i = 0; i < Length; ++i)
+  {
+    // Generate Random No in the Range 0 to Size of Input Variables
+    int RandomIndex = rand() % (temp.length());
+
+    PassWord+=temp[RandomIndex];
+
+  }
+
+  //Returning PassWord
+  return PassWord;
+}
+//Main
+int main() {
+
+  // Providing a seed value
+  srand((unsigned) time(NULL));
+  
+  //Length Of Password
+  int Length;cin>>Length;
+
+  //Random Password
+  cout<<Password_Generator(Length)<<endl;
+  
+}
